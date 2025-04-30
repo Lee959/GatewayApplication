2c7dc900b991d4bf1512128d54837d20147aad3b
Shuting <shutingli8999@gmail.com>
2025-04-30 16:57:50 +0800

    04.30 F

--- app/src/main/java/com/example/gatewayapplication/GatewayActivity.java
+++ app/src/main/java/com/example/gatewayapplication/GatewayActivity.java
@@ -1,9 +1,15 @@
 package com.example.gatewayapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.LinkedList;
@@ -20,15 +26,37 @@ public class GatewayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        context = GatewayActivity.this;
+//        context = GatewayActivity.this;
         bindViews();
         gateawayData = new LinkedList<Gateaway>();
-        gateawayAdapter = new GateawayAdapter((LinkedList<Gateaway>) gateawayData,context);
+        gateawayAdapter = new GateawayAdapter((LinkedList<Gateaway>) gateawayData, context);
         gatewayListView.setAdapter(gateawayAdapter);
+
+
+        gatewayListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
 
     }
 
-    private void bindViews(){
+    private void bindViews() {
         gatewayListView = (ListView) findViewById(R.id.gatewayListView);
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        // Get fragment manager
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // Begin fragment transaction
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        // Replace fragment and commit
+        fragmentTransaction.replace(R.id.connectedDevice, fragment);
+        fragmentTransaction.commit();
     }
 }
