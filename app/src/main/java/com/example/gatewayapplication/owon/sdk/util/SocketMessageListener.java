45e495b8ee25917bbc7730c68b3d31019d6c6e27
Shuting <shutingli8999@gmail.com>
2025-05-06 07:32:57 +0800

    Initial commit

--- app/src/main/java/com/example/gatewayapplication/owon/sdk/util/SocketMessageListener.java
+++ app/src/main/java/com/example/gatewayapplication/owon/sdk/util/SocketMessageListener.java
@@ -1,4 +1,18 @@
-package com.example.gatewayapplication.owon.sdk.util;
+package owon.sdk.util;
 
-public class SocketMessageListener {
+/**
+ * Socket message listener interface for receiving callbacks from the server.
+ * This interface defines the method for receiving messages from devices and gateways.
+ */
+public interface SocketMessageListener {
+
+    /**
+     * Callback method for receiving messages from devices and gateways.
+     *
+     * @param commandID The command ID indicating the type of message.
+     *                  This ID corresponds to the constants defined in the Constants class.
+     * @param bean      The data bean containing the response data.
+     *                  This bean needs to be cast to the appropriate device-specific subclass.
+     */
+    void getMessage(int commandID, Object bean);
 }
